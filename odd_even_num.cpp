e35ed83e54b24342862aba43bd79cf30c456bb4d
m1cr0n-00 <118296216+m1cr0n-00@users.noreply.github.com>
2022-11-23 07:45:22 +0000

    Create odd_even_num.cpp

---
+++ odd_even_num.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a;
+	
+	
+	printf ("Enter a number:");
+	scanf  ("%d",&a);
+	
+	if (a % 2){
+
+		printf ("The number is odd");
+	}else{
+		printf ("The number is even");
+	}
+	
+
+	
+	
+	
+	
+}
