0a6fc8dc03026a8f50db6069f29e83606cc208a3
m1cr0n-00 <118296216+m1cr0n-00@users.noreply.github.com>
2022-11-23 07:52:48 +0000

    Create generate_eng_alpha.cpp

---
+++ generate_eng_alpha.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+
+
+int main() {
+	
+	
+	char str;
+	
+	
+	for (str=65;str<90;str++) {
+		printf ("%c",str);
+	}
+	
+	for (str=97;str<122;str++) {
+		printf ("%c",str);
+	}
+	
+	return 0;
+	
+}
