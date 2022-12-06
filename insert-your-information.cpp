b36a0dfdb540258dfe5b2de9db00751ab169f165
Jozmon <118296216+Jozmon@users.noreply.github.com>
2022-12-06 21:21:50 +0000

    Create insert-your-information.cpp

---
+++ insert-your-information.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int age;
+int pnumber;
+int name;
+
+
+int main() {
+
+printf ("Write name:");
+scanf  ("%s",name);
+
+printf ("Write age:");
+scanf  ("%d",&age);
+
+printf ("Write phone number:");
+scanf  ("%d",&number);
+
+
+printf ("Your name is:%c",name);
+printf ("Your age is:%d",age);
+printf ("Your phone number is:%d",pnumber);
+
+
+return 0;
+
+}
